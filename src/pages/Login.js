9d82ef7204a33a3f19a7ad18de001f2f1de485f9
Diushan <forum_st03@forum.ac.jp>
2025-06-19 13:29:04 +0900

    login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import logo from '../assets/logo.png'; // adjust path as needed
-
+// styles
 import {
   Form,
   Input,
